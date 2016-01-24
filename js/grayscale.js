540a8d62a8544003dc7800745dd7fd6b0d33922a
Josh <millsjb@live.unc.edu>
2016-01-23 21:25:51 -0500

    updated map

--- js/grayscale.js
+++ js/grayscale.js
@@ -33,6 +33,8 @@ $('.navbar-collapse ul li a').click(function() {
 // When the window has finished loading create our google map below
 google.maps.event.addDomListener(window, 'load', init);
 
+
+/*! 
 function init() {
     // Basic options for a simple Google Map
     // For more options see: https://developers.google.com/maps/documentation/javascript/reference#MapOptions
@@ -175,4 +177,56 @@ function init() {
         map: map,
         icon: image
     });
+    */
+    
+    function init() {
+				//var mapCanvas = document.getElementById('map');
+				
+				var myLatLng = {lat: 35.919576, lng: -79.053618};
+				
+				var mapOptions = {
+					zoom: 14,
+					center: myLatLng,
+					mapTypeId: google.maps.MapTypeId.ROADMAP,
+					zoomControl: true,
+					zoomControlOptions: {
+						position: google.maps.ControlPosition.RIGHT_BOTTOM
+					},
+					mapTypeControl: true,
+					mapTypeControlOptions: {
+						style: google.maps.MapTypeControlStyle.DROPDOWN_MENU,
+						position: google.maps.ControlPosition.RIGHT_TOP
+					},
+					scaleControl: true,
+					streetViewControl: true,
+					streetViewControlOptions: {
+						position: google.maps.ControlPosition.RIGHT_BOTTOM
+					},
+					rotateControl: false
+				}
+				
+				var map = new google.maps.Map(document.getElementById('map'), mapOptions);
+				
+				var marker = new google.maps.Marker({
+					position: myLatLng,
+					map: map,
+					title: 'Statesville High School'
+				});
+				
+				var trafficLayer = new google.maps.TrafficLayer();
+				trafficLayer.setMap(map);
+				
+				var contentString = '<div id="content">'+
+				'<h1>Statesville High School</h1>'+
+				'</div>';
+				
+				var infowindow = new google.maps.InfoWindow({
+					content: contentString
+				});
+				
+				marker.addListener('click', function() {
+					infowindow.open(map, marker);
+				});
+			}
+			google.maps.event.addDomListener(window, 'load', init);
 }
