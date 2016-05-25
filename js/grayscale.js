92c8bdd897ffcf9d5fa956f05020dbf710a3ee5c
rizky2323 <rizky2323@gmail.com>
2016-05-25 21:31:06 +0700

    Update grayscale.js

--- js/grayscale.js
+++ js/grayscale.js
@@ -175,7 +175,7 @@ function init() {
 
     // Custom Map Marker Icon - Customize the map-marker.png file to customize your icon
     var image = 'img/map-marker.png';
-    var myLatLng = new google.maps.LatLng(40.6700, -73.9400);
+    var myLatLng = new google.maps.LatLng(0.468067, 101.355946);
     var beachMarker = new google.maps.Marker({
         position: myLatLng,
         map: map,
